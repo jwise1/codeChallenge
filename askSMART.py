2704acdff3706d6ecb0c960d12129228dbf50011
jwise1 <91438705+jwise1@users.noreply.github.com>
2023-12-08 21:02:29 -0600

    Update askSMART.py

--- askSMART.py
+++ askSMART.py
@@ -49,42 +49,42 @@ def formQuestion(message,newMessage,i):
 
 def followUp(newMessage,message,i):
     if i==1:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""more specifically? Yes or no."""
+        prompt="""Does this answer: """+newMessage+""" address this goal: """+message+""" more specifically? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask more specifically how this goal can be broken down into smaller steps:"""+message
+            prompt="""Write a question to ask more specifically how this goal can be broken down into smaller steps: """+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
             #followUp(newMessage,message,i)
     if i==2:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""in a measured way? Yes or no."""
+        prompt="""Does this answer: """+newMessage+""" address this goal: """+message+""" in a measured way? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask how to better measure this goal quantifiably:"""+message
+            prompt="""Write a question to ask how to better measure this goal quantifiably: """+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
             #followUp(newMessage,message,i)
     if i==3:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with how it can be achieved? Yes or no."""
+        prompt="""Does this answer: """+newMessage+""" address this goal: """+message+""" with how it can be achieved? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask how this goal can be achieved through actions:"""+message
+            prompt="""Write a question to ask how this goal can be achieved through actions: """+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
             #followUp(newMessage,message,i)
     if i==4:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""relevantly to a human? Yes or no."""
+        prompt="""Does this answer: """+newMessage+""" address this goal: """+message+""" relevantly to a human? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask how this goal can be relevantly addressed in one's life:"""+message
+            prompt="""Write a question to ask how this goal can be relevantly addressed in one's life: """+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
             #followUp(newMessage,message,i)
     if i==5:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with time considerations? Yes or no."""
+        prompt="""Does this answer: """+newMessage+""" address this goal: """+message+""" with time considerations? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask how this goal can be more timely thought of in terms of weeks, months and years:"""+message
+            prompt="""Write a question to ask how this goal can be more timely thought of in terms of weeks, months and years: """+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
             #followUp(newMessage,message,i)
