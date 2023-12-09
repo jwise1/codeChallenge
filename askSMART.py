0fb596e98866c523f66aff09c5a3b46a0187ddf3
Jared <wise.jared27@gmail.com>
2023-12-08 19:52:59 -0600

    COMMIT_EDITMSG

--- askSMART.py
+++ askSMART.py
@@ -17,78 +17,83 @@ def talkToUser(message):
     userIn=input(message+"\n")
     return userIn
 
-def formQuestion(message,i):
+def formQuestion(message,newMessage,i):
     initQ="What is one of your current goals?"
     if i==0:
         message=talkToUser(initQ)
         newMessage=message
     else:
         if i==1:
-            prompt="""Write a question about breaking down the following goal more specifically: """+message
+            prompt="""Write a question about breaking down the following goal more specifically: """+message+""" while also considering """+newMessage
             q=pipe(prompt,max_new_tokens=25,eos_token_id=tokenizer.eos_token_id)
             newMessage=talkToUser(q)
         if i==2:
-            prompt="""Write a question about how to measure the goal: """+message
+            prompt="""Write a question about how to measure the goal: """+message+""" while also considering """+newMessage
             q=pipe(prompt,max_new_tokens=25,eos_token_id=tokenizer.eos_token_id)
             newMessage=talkToUser(q)
         if i==3:
-            prompt="""Write a question about how achievable the goal is: """+message
+            prompt="""Write a question about how achievable the goal is: """+message+""" while also considering """+newMessage
             q=pipe(prompt,max_new_tokens=25,eos_token_id=tokenizer.eos_token_id)
             newMessage=talkToUser(q)
         if i==4:
-            prompt="""Write a question about how relevant the goal is overall: """+message
+            prompt="""Write a question about how relevant the goal is overall: """+message+""" while also considering """+newMessage
             q=pipe(prompt,max_new_tokens=25,eos_token_id=tokenizer.eos_token_id)
             newMessage=talkToUser(q)
         if i==5:
-            prompt="""Write a question about the how much time it will take to complete the goal: """+message
+            prompt="""Write a question about the how much time it will take to complete the goal: """+message+""" while also considering """+newMessage
             q=pipe(prompt,max_new_tokens=25,eos_token_id=tokenizer.eos_token_id)
             newMessage=talkToUser(q)
 
         followUp(newMessage,message,i)
-    return message
+    return message,newMessage
 
 def followUp(newMessage,message,i):
     if i==1:
         prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""more specifically? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask more specifically how this goal can be broken down:"""+message
+            prompt="""Write a question to ask more specifically how this goal can be broken down into smaller steps:"""+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
-
+            #followUp(newMessage,message,i)
     if i==2:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with measurability? Yes or no."""
+        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""in a measured way? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask how to better measure this goal:"""+message
+            prompt="""Write a question to ask how to better measure this goal quantifiably:"""+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
+            #followUp(newMessage,message,i)
     if i==3:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with how to achieve it? Yes or no."""
+        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with how it can be achieved? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask more achievably how this goal can be broken down:"""+message
+            prompt="""Write a question to ask how this goal can be achieved through actions:"""+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
+            #followUp(newMessage,message,i)
     if i==4:
-        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""relevantly? Yes or no."""
+        prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""relevantly to a human? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask more relevantly how this goal can be broken down:"""+message
+            prompt="""Write a question to ask how this goal can be relevantly addressed in one's life:"""+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
+            #followUp(newMessage,message,i)
     if i==5:
         prompt="""Does this answer: """+newMessage+"""address this goal:"""+message+"""with time considerations? Yes or no."""
         q=pipe(prompt,max_new_tokens=25)
         if q[0]['generated_text']=="No":
-            prompt="""Write a question to ask more timely how this goal can be broken down:"""+message
+            prompt="""Write a question to ask how this goal can be more timely thought of in terms of weeks, months and years:"""+message
             q=pipe(prompt,max_new_tokens=25)
             newMessage=talkToUser(q)
+            #followUp(newMessage,message,i)
 
 def main():
     resp=""
-    for i in range(0,6):
-        resp=formQuestion(resp,i)
+    message=""
+    for i in range(0,5):
+        message, resp=formQuestion(message,resp,i)
     
 if __name__ == "__main__":
     main()
